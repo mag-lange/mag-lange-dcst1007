e92c80a3820be038b4884860ffc59feca43db5fa
Magnus Lange <magnolan@stud.ntnu.no>
2024-01-15 09:58:03 +0100

    some code

--- OOP1/OOP1.js
+++ OOP1/OOP1.js
@@ -1,1 +1,22 @@
-//Hi, this is the first file, and I am pushing the change to github
+//Code for OOP1.html
+
+class car {
+    constructor(registration, brand, yearModel, speed) {
+        this.registration = registration
+        this.brand = brand
+        this.yearModel = yearModel
+        this.speed = speed
+    }
+
+    accelerate() {
+        console.log("accelerating...")
+    }
+
+}
+
+let volvo = new car("BT8713", "volvo", "2005", "50");
+let BMW = new car("ZZ1234", "BMW", "2020", "100");
+let toyota = new car("EB1829", "toyota", "2019", "80");
+let ferrari = new car("BD8938", "ferrari", "2023", "200"); 
+
+console.log(volvo.yearModel)
