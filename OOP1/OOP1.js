1fed84b14da53c1844bcb0e0744195cb41083671
Magnus Lange <magnolan@stud.ntnu.no>
2024-01-15 10:55:52 +0100

    Simple gas and brake added

--- OOP1/OOP1.js
+++ OOP1/OOP1.js
@@ -1,22 +1,48 @@
 //Code for OOP1.html
-
-class car {
-    constructor(registration, brand, yearModel, speed) {
+class Car {
+    constructor(registration, brand, yearModel, speed, max_speed) {
         this.registration = registration
         this.brand = brand
         this.yearModel = yearModel
         this.speed = speed
+        this.max_speed = max_speed
     }
-
     accelerate() {
-        console.log("accelerating...")
+        console.log("accelerating " + this.brand +"..." )
+        this.speed = this.speed + 10
+        return this.speed
     }
-
+    brake() {
+        console.log("braking..."+ this.brand +"..." )
+        this.speed = this.speed - 10
+        return this.speed
+    }
 }
 
-let volvo = new car("BT8713", "volvo", "2005", "50");
-let BMW = new car("ZZ1234", "BMW", "2020", "100");
-let toyota = new car("EB1829", "toyota", "2019", "80");
-let ferrari = new car("BD8938", "ferrari", "2023", "200"); 
+let cars = [
+    volvo = new Car("BT8713", "volvo", "2005", 0, 100),
+    bmw = new Car("ZZ1234", "BMW", "2020",0, 150),
+    toyota = new Car("EB1829", "toyota", "2019", 0, 60),
+    ferrari = new Car("BD8938", "ferrari", "2023", 0, 200)
+]
 
-console.log(volvo.yearModel)
+let accelerate_button = document.getElementById("accelerate")
+accelerate_button.onclick = () => {
+    let carSelection = document.getElementById("carSelection")
+    let selectedCar = cars[carSelection.value]; // A bit complex, but I did not find any better solution
+    selectedCar.accelerate();
+    console.log(selectedCar.speed);
+}
+
+let brake_button = document.getElementById("brake")
+brake_button.onclick = () => {
+    let carSelection = document.getElementById("carSelection")
+    let selectedCar = cars[carSelection.value]; // A bit complex, but I did not find any better solution
+    selectedCar.brake();
+    console.log(selectedCar.speed);
+}
+
+
+
+
+
