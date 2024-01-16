ed99cdb98606294c3b40f83320a40f71b0a32d72
Magnus Lange <magnolan@stud.ntnu.no>
2024-01-16 15:24:50 +0100

    finsihed

--- OOP1/OOP1.js
+++ OOP1/OOP1.js
@@ -1,25 +1,4 @@
 //Code for OOP1.html
-//global variables
-let x = 0
-setInterval(() => {
-    x = x%4
-    let speed = cars[x].speed
-    switch (x) {
-        case 0:
-            document.getElementById("speed_volvo").innerHTML = speed
-            break;
-        case 1:
-            document.getElementById("speed_bmw").innerHTML = speed
-            break;
-        case 2:
-            document.getElementById("speed_toyota").innerHTML = speed
-            break;
-        case 3:
-            document.getElementById("speed_ferrari").innerHTML = speed
-            break;
-    }
-    x++
-},100)
 class Car {
     constructor(registration, brand, yearModel, speed, max_speed) {
         this.registration = registration
@@ -29,7 +8,6 @@ class Car {
         this.max_speed = max_speed
     }
     accelerate() {
-        console.log("accelerating " + this.brand +"..." )
         this.speed = this.speed + 10
         if(this.speed >= this.max_speed) {
             this.speed = this.max_speed
@@ -38,7 +16,6 @@ class Car {
         return this.speed
     }
     brake() {
-        console.log("braking..."+ this.brand +"..." )
         this.speed = this.speed - 10
         if (this.speed < 0) {
             this.speed = 0
@@ -55,12 +32,86 @@ let cars = [
     ferrari = new Car("BD8938", "ferrari", "2023", 0, 200)
 ]
 
+let x = 0
+let backwards_v = false
+let backwards_b = false
+let backwards_t = false
+let backwards_f = false
+let volvo_img = document.getElementById("volvo_car")
+let bmw_img = document.getElementById("bmw_car")
+let toyota_img = document.getElementById("toyota_car")
+let ferrari_img = document.getElementById("ferrari_car")
+
+setInterval(() => {
+    x = x%4
+    let speed = cars[x].speed
+    switch (x) {
+        case 0:
+            document.getElementById("speed_volvo").innerHTML = speed
+            if (speed == 0) {break}
+            else if (volvo_img.offsetLeft < 1000 && backwards_v == false) {
+                volvo_img.style.left = (volvo_img.offsetLeft + speed) + 'px'
+            }
+            else {
+                backwards_v = true
+                volvo_img.style.left = (volvo_img.offsetLeft - speed) + 'px'
+                if (volvo_img.offsetLeft < 10) {
+                    backwards_v = false
+                }
+            }
+            break;
+        case 1:
+            document.getElementById("speed_bmw").innerHTML = speed   
+            if (speed == 0) {break}
+            else if (bmw_img.offsetLeft < 1000 && backwards_b == false) {
+                bmw_img.style.left = (bmw_img.offsetLeft + speed) + 'px'
+            }
+            else {
+                backwards_b = true
+                bmw_img.style.left = (bmw_img.offsetLeft - speed) + 'px'
+                if (bmw_img.offsetLeft < 10) {
+                    backwards_b = false
+                }
+            }  
+            break;
+        case 2:
+            document.getElementById("speed_toyota").innerHTML = speed
+            if (speed == 0) {break}
+            else if (toyota_img.offsetLeft < 1000 && backwards_t == false) {
+                toyota_img.style.left = (toyota_img.offsetLeft + speed) + 'px'
+            }
+            else {
+                backwards_t = true
+                toyota_img.style.left = (toyota_img.offsetLeft - speed) + 'px'
+                if (toyota_img.offsetLeft < 10) {
+                    backwards_t = false
+                }
+            }  
+            break;
+        case 3:
+            document.getElementById("speed_ferrari").innerHTML = speed
+            if (speed == 0) {break}
+            else if (ferrari_img.offsetLeft < 1000 && backwards_f == false) {
+                ferrari_img.style.left = (ferrari_img.offsetLeft + speed) + 'px'
+            }
+            else {
+                backwards_f = true
+                ferrari_img.style.left = (ferrari_img.offsetLeft - speed) + 'px'
+                if (ferrari_img.offsetLeft < 10) {
+                    backwards_f = false
+                }
+            }  
+            break;
+    }
+    x++
+},1)
+
+
 let accelerate_button = document.getElementById("accelerate")
 accelerate_button.onclick = () => {
     let carSelection = document.getElementById("carSelection")
     let selectedCar = cars[carSelection.value]; // A bit complex, but I did not find any better solution
     selectedCar.accelerate();
-    console.log(selectedCar.speed);
 }
 
 let brake_button = document.getElementById("brake")
@@ -68,8 +119,8 @@ brake_button.onclick = () => {
     let carSelection = document.getElementById("carSelection")
     let selectedCar = cars[carSelection.value]; // A bit complex, but I did not find any better solution
     selectedCar.brake();
-    console.log(selectedCar.speed);
 }
+
 
 
 
