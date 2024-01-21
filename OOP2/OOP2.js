258d9e7c6c97483b42ce803954b250b0eaa221ff
Magnus Lange <magnolan@stud.ntnu.no>
2024-01-21 10:21:39 +0100

    initial structure

---
+++ OOP2/OOP2.js
@@ -0,0 +1,18 @@
+//Script for the tasks to be delivered in week 4
+
+class AccountInfo {
+    constructor ( clientNumber, clientName, amount ) {
+        clientNumber = this.clientNumber;
+        clientName = this.clientName
+        amount = this.amount 
+    }
+
+    amountNow(amount) {
+        amount = this.amount 
+        return amount
+    }
+}
+
+class ChildAccount extends AccountInfo {
+    
+}
