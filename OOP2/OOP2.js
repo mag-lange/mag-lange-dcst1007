f6d7b95790c3de6cffa1d851b7cbf49293b151c4
Magnus Lange <magnolan@stud.ntnu.no>
2024-01-21 11:01:41 +0100

    task 1-4

--- OOP2/OOP2.js
+++ OOP2/OOP2.js
@@ -1,18 +1,46 @@
 //Script for the tasks to be delivered in week 4
 
-class AccountInfo {
+class BankAccount {
     constructor ( clientNumber, clientName, amount ) {
-        clientNumber = this.clientNumber;
-        clientName = this.clientName
-        amount = this.amount 
+        this.clientNumber = clientNumber;
+        this.clientName = clientName
+        this.amount  = parseInt(amount) 
     }
+    amountNow() {
+        return this.amount
+    }
+    withdraw(withdrawAmount) {
+        this.withdrawAmount = withdrawAmount
+        if (this.amount < this.withdrawAmount) {
+                return console.log(this.clientName + " does not have enough money to withdraw this amount")
+        }
+        else {
+            this.amount = (this.amount - withdrawAmount)
+        }
+        return console.log("New amount: " + this.amountNow())
+    }
+    deposit(depositAmount) {
+        this.depositAmount = depositAmount
+        this.amount = (this.amount + depositAmount)
+        return console.log("New amount: " + this.amountNow()) // We have no way of verifying if the person has the equity to deposit
+    }
+    accountInfo() {
+        return console.log(this.clientName + " with client number " + this.clientNumber + " has " + this.amount + "$ available")
+    }
+}
+class ChildAccount extends BankAccount {
+    constructor ( clientNumber, clientName, age) {
+        super( clientNumber, clientName)
+        this.age = age
+        this.amount = 200        
+        this.age = age
 
-    amountNow(amount) {
-        amount = this.amount 
-        return amount
+        if (age > 3) {
+            console.log("error, cannot make child account, age to high")
+            this.amount = 0
+        }
     }
 }
 
-class ChildAccount extends AccountInfo {
-    
-}
+let jens = new ChildAccount(4, "Jens", 6)
+let kari = new BankAccount(92349, "Kari", 600)
