cceb0c421aae483af72fb1f5f96ad76566666ff8
Magnus Lange <magnolan@stud.ntnu.no>
2024-01-24 13:06:08 +0100

    All obligatory OOP2 done

--- OOP2/OOP2.js
+++ OOP2/OOP2.js
@@ -1,12 +1,14 @@
 //Script for the tasks to be delivered in week 4
 
+
 class BankAccount {
     constructor ( clientNumber, clientName, amount ) {
         this.clientNumber = clientNumber;
         this.clientName = clientName
-        this.amount  = parseInt(amount) 
+        this.amount  = parseInt(amount) //Feature that means you can write in 200 or "200"
     }
     amountNow() {
+        this.accountInfo()
         return this.amount
     }
     withdraw(withdrawAmount) {
@@ -16,16 +18,21 @@ class BankAccount {
         }
         else {
             this.amount = (this.amount - withdrawAmount)
+        
+        this.accountInfo()
+        return console.log("Withdrawal of " + withdrawAmount + "$ from " + this.clientName + " successful. New amount: " + this.amountNow() + "$")
         }
-        return console.log("New amount: " + this.amountNow())
     }
     deposit(depositAmount) {
         this.depositAmount = depositAmount
         this.amount = (this.amount + depositAmount)
-        return console.log("New amount: " + this.amountNow()) // We have no way of verifying if the person has the equity to deposit
+        this.accountInfo()
+        return console.log("Money on its way! " + this.clientName + " has received " + depositAmount + "$, New amount: " + this.amountNow() + "$") 
     }
     accountInfo() {
-        return console.log(this.clientName + " with client number " + this.clientNumber + " has " + this.amount + "$ available")
+        console.log(this.clientName + " with client number " + this.clientNumber + " has " + this.amount + "$ available")
+
+        return document.getElementById(this.clientNumber.toString()).innerHTML = this.amount + "$"
     }
 }
 class ChildAccount extends BankAccount {
@@ -36,11 +43,54 @@ class ChildAccount extends BankAccount {
         this.age = age
 
         if (age > 3) {
-            console.log("error, cannot make child account, age to high")
+            console.log("error, cannot get child bonus, age to high")
             this.amount = 0
         }
     }
 }
 
-let jens = new ChildAccount(4, "Jens", 6)
-let kari = new BankAccount(92349, "Kari", 600)
+let lise_jensen = new ChildAccount(9294, "Lise Jensen", 2)
+let kari_hansen = new BankAccount(1234, "Kari Hansen", 895)
+let petter_olsen = new BankAccount(3311, "Petter Olsen", 0)
+let simulate = document.getElementById("simulate")
+lise_jensen.accountInfo()
+kari_hansen.accountInfo()
+petter_olsen.accountInfo()
+
+simulate.onclick = () => {
+    
+    let time_now = new Date();
+    time_now.setHours(7, 0, 0, 0);
+    let time_html = document.getElementById("time")
+    let form_time
+    
+    let bank_simulation = setInterval(() => {
+        time_now.setTime(time_now.getTime() + 60000);
+        form_time = `${time_now.getHours().toString().padStart(2, '0')}:${time_now.getMinutes().toString().padStart(2, '0')}` 
+        //padstart function from chatGPT
+        time_html.innerText = "The time is - " + form_time;
+
+        switch(form_time.toString()) {
+                case "10:30":
+                    kari_hansen.withdraw(300)
+                    break;
+                case "11:00":
+                    lise_jensen.deposit(4000)
+                    petter_olsen.deposit(3000)
+                    break
+                case "12:15":
+                    kari_hansen.withdraw(250)
+                    petter_olsen.deposit(250)
+                    break
+                case "17:30":
+                    kari_hansen.withdraw(800)
+                    break
+                case "23:00":
+                    console.log("Bank is closing now, goodnight")
+                    clearInterval(bank_simulation)
+                    return
+                default:
+                    break
+        }
+    }, 1);
+    }
