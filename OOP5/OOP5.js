267b3a57a6b206a5e75c6a48065fbb1487951950
magnolan@stud.ntnu.no <magnolan@stud.ntnu.no>
2024-02-13 14:09:10 +0100

    all obligatory done

--- OOP5/OOP5.js
+++ OOP5/OOP5.js
@@ -1,14 +1,14 @@
 //Script file for OOP5.html and using the JSON file mountains.json
-
 //I hardcoded the json file, maybe not the best idea
+
   fetch('mountains.json')
     .then(response => response.json())
     .then(data => {
+      data.fjelltopper.sort((a, b) => b.elevation - a.elevation) //Sort in descending order
+      //I took this smart method from https://www.w3schools.com/js/js_array_sort.asp 
       const mountainsTable = document.getElementById('mountainsTable');
       const tbody = mountainsTable.querySelector('tbody');
-
-      // Iterate over the mountains data and create table rows
-      data.fjelltopper.forEach(mountain => {
+      data.fjelltopper.forEach(mountain => { //Syntax learned in DCST1005 for powershell
         const row = document.createElement('tr');
         row.innerHTML = `
           <td>${mountain.fylke}</td>
